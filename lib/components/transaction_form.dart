83341a0c6f0f7155bfd12590d0a2d340c1fbd857
André Ary Castelo <andreary599@gmail.com>
2025-10-30 20:19:54 -0300

    adding datepicker

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
@@ -10,19 +11,36 @@ class TransactionForm extends StatefulWidget {
 }
 
 class _TransactionFormState extends State<TransactionForm> {
-  final titleControler = TextEditingController();
-
-  final valueControler = TextEditingController();
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+  DateTime? _selectedDate;
 
   _submitForm() {
-    final title = titleControler.text;
-    final value = double.tryParse(valueControler.text) ?? 0.0;
+    final title = _titleController.text;
+    final value = double.tryParse(_valueController.text) ?? 0.0;
 
     if (title.isEmpty || value <= 0) {
       return;
     }
 
     widget.onSubmit(title, value);
+  }
+
+  _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2024),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 
   @override
@@ -37,7 +55,7 @@ class _TransactionFormState extends State<TransactionForm> {
               padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: TextField(
                 onSubmitted: (_) => _submitForm(),
-                controller: titleControler,
+                controller: _titleController,
                 decoration: InputDecoration(
                   labelText: 'Título',
                   labelStyle: TextStyle(fontSize: 20),
@@ -49,7 +67,7 @@ class _TransactionFormState extends State<TransactionForm> {
               padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: TextField(
                 onSubmitted: (_) => _submitForm(),
-                controller: valueControler,
+                controller: _valueController,
                 decoration: InputDecoration(
                   labelText: 'Valor (R\$)',
                   labelStyle: TextStyle(fontSize: 20),
@@ -58,19 +76,44 @@ class _TransactionFormState extends State<TransactionForm> {
                 style: TextStyle(fontSize: 20),
               ),
             ),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? "Nenhuma data selecionada"
+                          : 'Data Selecionada: ${DateFormat('dd/MM/y').format(_selectedDate!)}',
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Theme.of(context).primaryColor,
+                    ),
+                    child: Text(
+                      "Selecionar Data",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: _showDatePicker,
+                  ),
+                ],
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   FloatingActionButton.extended(
+                    foregroundColor: Theme.of(context).primaryColor,
                     label: SizedBox(
                       width: 100,
                       height: 60,
                       child: Center(
                         child: Text(
                           'Nova Transação',
-                          style: TextStyle(fontSize: 16),
+                          style: TextStyle(fontSize: 16, color: Colors.white),
                         ),
                       ),
                     ),
