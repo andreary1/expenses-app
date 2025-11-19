34f07cfb5db76f6fc1645dc2bcbb2c7b929c4288
André Ary Castelo <andreary599@gmail.com>
2025-11-19 10:29:09 -0300

    delete transaction

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -3,8 +3,9 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List transactions;
+  final void Function(String) onRemove;
 
-  const TransactionList(this.transactions, {super.key});
+  const TransactionList(this.transactions, this.onRemove, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +45,11 @@ class TransactionList extends StatelessWidget {
                       style: Theme.of(context).textTheme.titleLarge,
                     ),
                     subtitle: Text(DateFormat('d MMM y').format(tr.date)),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).colorScheme.error,
+                      onPressed: () => onRemove(tr.id),
+                    ),
                   ),
                 );
               },
--- lib/main.dart
+++ lib/main.dart
@@ -71,6 +71,12 @@ class _MyHomePageState extends State<MyHomePage> {
     Navigator.of(context).pop();
   }
 
+  _removeTransaction(String id) {
+    setState(() {
+      _transactions.removeWhere((tr) => tr.id == id);
+    });
+  }
+
   _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
       context: context,
@@ -97,7 +103,10 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [Chart(_transactions), TransactionList(_transactions)],
+          children: [
+            Chart(_transactions),
+            TransactionList(_transactions, _removeTransaction),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
